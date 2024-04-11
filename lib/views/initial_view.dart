eb5d089d6e983d2b8d602717ff8e59360c46cee8
renatolomba <rntlomba@gmail.com>
2024-04-11 20:40:30 -0300

    showing videos on list

--- lib/views/initial_view.dart
+++ lib/views/initial_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_clone/api.dart';
+import 'package:youtube_clone/models/video.dart';
 
 class InitialView extends StatefulWidget {
   const InitialView({super.key});
@@ -11,10 +12,60 @@ class InitialView extends StatefulWidget {
 class _InitialViewState extends State<InitialView> {
   final Api api = Api();
 
+  Future<List<Video>> _getVideos() async => api.search('');
+
   @override
   Widget build(BuildContext context) {
-    api.search('');
+    return FutureBuilder<List<Video>>(
+      future: _getVideos(),
+      builder: (_, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+          case ConnectionState.waiting:
+            return const Center(
+              child: CircularProgressIndicator(color: Colors.red)
+            );
+          case ConnectionState.active:
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return const Text('Erro ao buscar vídeos.');
+            } else if (snapshot.data == null) {
+              return const Text('Nenhum dado a ser exibido');
+            }
 
-    return const Center(child: Text('Início'));
+            var videos = snapshot.data!;
+
+            return ListView.separated(
+              padding: const EdgeInsets.all(16),
+              itemCount: videos.length,
+              separatorBuilder: (_, idx) => const Divider(
+                height: 2,
+                color: Colors.grey,
+              ),
+              itemBuilder: (_, idx) {
+                var video = videos[idx];
+
+                return Column(
+                  children: [
+                    Container(
+                      height: 200,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(video.thumbnailUrl),
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      title: Text(video.title),
+                      subtitle: Text(video.channel),
+                    )
+                  ],
+                );
+              },
+            );
+        }
+      }
+    );
   }
 }
